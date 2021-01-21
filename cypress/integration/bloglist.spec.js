309852b30af014504a7a51165c09fa1f4471d634
Cyriacus Ifeanyi <cyriacusifeanyi@gmail.com>
2021-01-21 22:06:07 +0100

    5.19: bloglist end to end testing, step3

--- cypress/integration/bloglist.spec.js
+++ cypress/integration/bloglist.spec.js
@@ -1,4 +1,4 @@
-describe('Blog app', function () {
+describe('5.17: bloglist end to end testing, step1', function () {
   beforeEach(function () {
     cy.request('POST', 'http://localhost:3001/api/testing/reset')
     cy.visit('http://localhost:3000')
@@ -10,7 +10,7 @@ describe('Blog app', function () {
   })
 })
 
-describe('Blog app - logging in ', function () {
+describe('5.18: bloglist end to end testing, step2', function () {
   const newUser = {
     name: 'Thomas Aquinas',
     username: 'stThomas',
@@ -48,4 +48,51 @@ describe('Blog app - logging in ', function () {
 
     })
   })
+})
+
+describe('5.19: bloglist end to end testing, step3', function () {
+  // ...
+  const newUser = {
+    name: 'Thomas Aquinas',
+    username: 'stThomas',
+    password: '1991-1958'
+  }
+
+  const newBlog = {
+    title: 'Mr. Turning',
+    author: 'research@civm.com',
+    url: 'https://civm.com'
+  }
+
+  describe.only('When logged in', function () {
+    beforeEach(function () {
+      cy.request('POST', 'http://localhost:3001/api/testing/reset')
+      // create here a user to backend
+      cy.request('POST', 'http://localhost:3001/api/users/', newUser)
+      cy.visit('http://localhost:3000')
+
+      // log in user here
+      cy.get('#username').type(newUser.username)
+      cy.get('#password').type(newUser.password)
+      cy.get('#login-button').click()
+      // confirm that user is added
+      cy.contains(newUser.name.concat(' logged-in'))
+    })
+
+    it('A blog can be created', function () {
+      // open form
+      cy.get(':nth-child(1) > button').click()
+      // fill form
+      cy.get('#newBlogTitle').type(newBlog.title)
+      cy.get('#newBlogAuthor').type(newBlog.author)
+      cy.get('#newBlogUrl').type(newBlog.url)
+      // click submit
+      cy.get('#submitNewNote').click()
+
+      cy.get('.blog').should('contain', newBlog.title)
+
+
+    })
+  })
+
 })
