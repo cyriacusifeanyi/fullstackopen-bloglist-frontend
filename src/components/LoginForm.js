24d708c9e0b12b2f8c91375d8c71cdfab54dec59
Cyriacus Ifeanyi <cyriacusifeanyi@gmail.com>
2021-04-15 07:57:54 +0100

    completed 7.19 & 7.20: Styles, step 1 &2. I styled the login form with Tailwind CSS framework

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -9,35 +9,46 @@ const LoginForm = ({
   password
 }) => {
   return (
-    <form id='loginForm' onSubmit={handleSubmit}>
-      <h2>log in to application</h2>
 
-      <div>
-        username
-        <input
-          id='username'
-          type='text'
-          value={username}
-          name='username'
-          onChange={handleUsernameChange}
-          // onChange={({ target }) => setUsername(target.value)}
-          autoComplete='current-username'
-        />
+    <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
+      <div className="max-w-md w-full space-y-8">
+        <div>
+          <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+            log in to application
+          </h2>
+        </div>
+        <form id='loginForm' onSubmit={handleSubmit} className="mt-8 space-y-6">
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label htmlFor="username" className="sr-only">Username</label>
+              <input id="username" name="username" type="text" autoComplete="username" required
+                value={username} onChange={handleUsernameChange}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Username" />
+            </div>
+            <div>
+              <label htmlFor="password" className="sr-only">Password</label>
+              <input id="password" name="password" type="password" autoComplete="current-password" required
+                value={password} onChange={handlePasswordChange}
+                className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                placeholder="Password" />
+            </div>
+          </div>
+
+          <div>
+            <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+              <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                  <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                </svg>
+              </span>
+            Sign in
+            </button>
+          </div>
+        </form>
       </div>
-      <div>
-        password
-        <input
-          id='password'
-          type='password'
-          value={password}
-          name='password'
-          onChange={handlePasswordChange}
-          // onChange={({ target }) => setPassword(target.value)}
-          autoComplete='current-password'
-        />
-      </div>
-      <button id='login-button' type='submit'>login</button>
-    </form>
+    </div>
   )
 }
 
